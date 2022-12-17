3a448f95502dc6f5d2f4e439a65af73a0bf3b7e6
folegf76 <folegf76@gmail.com>
2022-12-17 16:14:35 +0200

    Add base WhyOur and Events

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import DishCategory, Dish
+from .models import DishCategory, Dish, WhyOur, Events
 
 # Register your models here.
 admin.site.register(DishCategory)
 admin.site.register(Dish)
+admin.site.register(WhyOur)
+admin.site.register(Events)
