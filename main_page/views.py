cb526b415ba8b42bc9e5943e2f680ebff28bb076
folegf76 <folegf76@gmail.com>
2022-12-18 12:08:48 +0200

    add events

--- main_page/views.py
+++ main_page/views.py
@@ -6,10 +6,16 @@ def main_page(request):
     categories = DishCategory.objects.filter(is_visible=True)
     dishes = Dish.objects.filter(is_visible=True, is_special=False)
     specials_dishes = Dish.objects.filter(is_visible=True, is_special=True)
+    about = WhyOur.objects.filter(is_visible=True, is_about=True)
+    why_us = WhyOur.objects.filter(is_visible=True, is_about=False)
+    events = Events.objects.filter(is_visible=True)
 
     return render(request, 'main_page.html', context={
         'categories': categories,
         'dishes': dishes,
         'special': specials_dishes,
+        'abouts': about,
+        'whyus': why_us,
+        'events': events,
     })
 
